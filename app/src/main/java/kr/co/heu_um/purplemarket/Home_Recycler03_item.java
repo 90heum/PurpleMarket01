043384400acdd60869305deb1f2fce2050bdc6cd
heum <792wjdgma@naver.com>
2021-02-10 13:24:56 +0900

    week recycler

---
+++ app/src/main/java/kr/co/heu_um/purplemarket/Home_Recycler03_item.java
@@ -0,0 +1,17 @@
+package kr.co.heu_um.purplemarket;
+
+public class Home_Recycler03_item {
+
+    int imgUrl;
+
+
+    public Home_Recycler03_item() {
+
+    }
+    public Home_Recycler03_item(int imgUrl) {
+        this.imgUrl = imgUrl;
+
+    }
+
+
+}
